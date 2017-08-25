0d6f21034f1c0dcf4b9e2c879eb3022c15d5abf5
Stanislav Starzhevskiy <s.starzhevskiy@redmadrobot.com>
2017-08-25 14:32:06 +0300

    AsYouTypeFormatter is now a protocol

--- StringMasker/StringMasker/TextFormatter.swift
+++ StringMasker/StringMasker/TextFormatter.swift
@@ -44,6 +44,19 @@ enum InputAction {
     case deleteLast(target: String)
     case deletion(target: String, range: Range<String.Index>)
     case insertion(string: String, target: String, range: Range<String.Index>)
+
+    var result: String {
+        switch self {
+            case .append(let string, let target):
+                return target.appending(string)
+            case .deleteLast(let target):
+                return target.substring(to: target.index(before: target.endIndex))
+            case .deletion(let target, let range):
+                return target.replacingCharacters(in: range, with: "")
+            case .insertion(let string, let target, let range):
+                return target.replacingCharacters(in: range, with: string)
+        }
+    }
 }
 
 enum CaretPosition {
@@ -56,18 +69,45 @@ struct FormatResult {
     var carretPosition: CaretPosition
 }
 
-struct AsYouTypeStringFormatter {
-    func format(existing: String, input: String, range: NSRange) -> String {
+protocol FormatDescriptor {
+    var pattern: String { get set }
+    var replacePattern: String { get set }
+    var toPlainPattern: String { get set }
+    var cleanPattern: String { get set }
+}
+
+protocol AsYouTypeStringFormatter {
+    func format(existing: String, input: String, range: NSRange) -> FormatResult
+    var descriptor: FormatDescriptor { get set }
+    var formatOnDeletion: Bool { get }
+    func format(_ input: InputAction) -> FormatResult
+}
+
+extension AsYouTypeStringFormatter {
+    func format(existing: String, input: String, range: NSRange) -> FormatResult {
         let action = inputAction(from: input, targetRange: range, target: existing)
-        return ""
+
+        switch action {
+            case .deleteLast, .deletion:
+                if formatOnDeletion {
+                    return format(action)
+                } else {
+                    let caretPosition = self.caretPosition(for: action)
+                    let actionResult = action.result
+                    return FormatResult(string: actionResult, carretPosition: caretPosition)
+                }
+            default:
+                return format(action)
+            }
+
     }
 
     func caretPosition(for inputAction: InputAction) -> CaretPosition {
         switch inputAction {
-        case .append:
-            return .end
-        case .deleteLast:
-            return .end
+            case .append:
+                return .end
+            case .deleteLast:
+                return .end
         // TODO: Logic for insertion / deletion
         case .deletion:
             return .end
@@ -78,17 +118,16 @@ struct AsYouTypeStringFormatter {
 
     func inputAction(from input: String, targetRange range: NSRange, target: String) -> InputAction {
         switch (range.length, input.characters.count) {
-            case (0, 1):
-                return .append(string: input, target: target)
-            case (1, 0):
-                return .deleteLast(target: target)
-            default:
-                if input.characters.count > 0 {
-                    return .insertion(string: input, target: target, range: range.toStringIndexRange(in: target))
-                } else {
-                    return .deletion(target: target, range: range.toStringIndexRange(in: target))
-                }
+        case (0, 1):
+            return .append(string: input, target: target)
+        case (1, 0):
+            return .deleteLast(target: target)
+        default:
+            if input.characters.count > 0 {
+                return .insertion(string: input, target: target, range: range.toStringIndexRange(in: target))
+            } else {
+                return .deletion(target: target, range: range.toStringIndexRange(in: target))
+            }
         }
     }
 }
-
